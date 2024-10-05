e0bce11bd638f02341a5139a84ee8910476f4db0
kade <bhpark93@gmail.com>
2024-10-05 14:00:25 +0900

    init

---
+++ src/test/java/study/me/empty/EmptyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.me.empty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
